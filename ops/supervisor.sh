79ccbee137d737f71625bfaf4140dedbe18641b3
nhatanh <blackheron02@gmail.com>
2024-04-15 16:28:49 +0700

    updated: supervisor startup script

--- ops/supervisor.sh
+++ ops/supervisor.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
+SVC_PATH="./target/release/request-receiver"
+WRK_PATH="./target/release/downstreamer"
+
 if [ ! -d ./run ]; then
   mkdir ./run
+fi
+
+if [ ! -e $SVC_PATH ] || [ ! -e $WRK_PATH ]; then
+  cargo build --release
 fi
 supervisord -c ops/supervisor.conf
