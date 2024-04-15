3f981beddcca229107c40cdc3c6a5cd3936f7425
nhatanh <blackheron02@gmail.com>
2024-04-15 16:51:41 +0700

    cleanup: flake.nix

--- flake.nix
+++ flake.nix
@@ -29,9 +29,9 @@
         linuxDeps = optionals isLinux [ inotify-tools ];
         defaultPkg = rustPlatform.buildRustPackage {
           pname =
-            toml.package.name; # make this what ever your cargo.toml package.name is
+            toml.package.name;
           version = "v${toml.package.version}";
-          src = ./.; # the folder with the cargo.toml
+          src = ./.;
 
           cargoLock.lockFile = ./Cargo.lock;
           cargoBuildFlags = [
@@ -69,28 +69,6 @@
               export PATH=$CARGO_HOME/bin:$PATH
               export LANG=C.UTF-8
             '';
-            #LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-            #shellHook = ''
-            #  export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
-            #  export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
-            #  '';
-            ## Add precompiled library to rustc search path
-            #RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
-            #  # add libraries here (e.g. pkgs.libvmi)
-            #]);
-            ## Add glibc, clang, glib and other headers to bindgen search path
-            #BINDGEN_EXTRA_CLANG_ARGS = 
-            ## Includes with normal include path
-            #(builtins.map (a: ''-I"${a}/include"'') [
-            #  # add dev libraries here (e.g. pkgs.libvmi.dev)
-            #  pkgs.glibc.dev 
-            #])
-            ## Includes with special directory paths
-            #++ [
-            #  ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-            #  ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-            #  ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
-            #];
           };
         };
       });
